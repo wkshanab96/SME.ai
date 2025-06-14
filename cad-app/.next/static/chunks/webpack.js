29acf9d708b640836efc2c33d5de1ae3d07a1e17
wkshanab96 <w.k.shanab96@gmail.com>
2025-06-14 13:45:35 -0400

    Refactor code structure for improved readability and maintainability

--- cad-app/.next/static/chunks/webpack.js
+++ cad-app/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "df39909d787f3d24"; }
+/******/ 		__webpack_require__.h = function() { return "3f5ec21285f0edb8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
